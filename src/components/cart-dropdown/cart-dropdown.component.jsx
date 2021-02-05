a6faf5c4c397f3d71fe42c686c771e095d37041f
gideon-chung <63383138+gideon-chung@users.noreply.github.com>
2021-02-05 14:41:24 -0500

    made changes to code using createStructuredSelector

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { createStructuredSelector } from 'reselect';
 
 import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
@@ -18,8 +19,8 @@ const CartDropdown = ({ cartItems }) => (
   </div>
 );
 
-const mapDispatchToProps = state => ({
-  cartItems: selectCartItems(state)
+const mapDispatchToProps = createStructuredSelector({
+  cartItems: selectCartItems
 });
 
 export default connect(mapDispatchToProps)(CartDropdown);
---
+++ src/redux/user/user.selectors.js
@@ -0,0 +1,15 @@
+import { createSelector } from "reselect";
+
+const selectUser = (state) => state.user;
+
+const selectCart = (state) => state.cart;
+
+export const selectCartHidden = createSelector(
+    [selectCart],
+    cart => cart.hidden
+)
+
+export const selectCurrentUser = createSelector(
+  [selectUser],
+  (user) => user.currentUser
+);
