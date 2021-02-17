d6ba6ea3a4f55987ea763a54ed2828acd95eecb4
gideon-chung <63383138+gideon-chung@users.noreply.github.com>
2021-02-17 14:29:01 -0500

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,8 +6,8 @@ import { createStructuredSelector } from 'reselect';
 import { auth } from "../../firebase/firebase.utils";
 import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
-import { selectCartHidden } from '../../redux/cart/cart.selectors';
-import { selectCurrentUser } from '../../redux/cart/cart.selectors';
+import { selectCartHidden } from '../../redux/user/user.selectors';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
